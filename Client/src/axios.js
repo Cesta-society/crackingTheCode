e7b4d48dbdf4d3166ce3a0757d920b825303514f
ARJIT <arjitbhandari222830@gmail.com>
2020-05-12 02:37:53 +0530

    Adding cors address

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://react-quiz123.herokuapp.com',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
