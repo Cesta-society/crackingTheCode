2d738df41539ae6767192fe575976e78e211de22
ARJIT <arjitbhandari222830@gmail.com>
2020-05-12 01:20:31 +0530

    Adding build

---
+++ Client/build/precache-manifest.6629c2de065a63aa232c8dd0efa78843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cf5cf2629d53565d91d46e08341b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f2e9e81d797e770221",
+    "url": "/static/js/2.0dacd762.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.0dacd762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9561bbf7a649eba665d1",
+    "url": "/static/js/main.10020ac5.chunk.js"
+  },
+  {
+    "revision": "b3d6010e6dacafdaf81e",
+    "url": "/static/js/runtime-main.66f8016c.js"
+  }
+]);
